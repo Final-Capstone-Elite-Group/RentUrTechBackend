7eff473e252b552eceab9f04a5c891ff51182a69
jose-Abel <joseabel.ramirezfrontany@gmail.com>
2022-06-02 13:44:14 -0400

    Fixing rubocop errors

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,13 +1,9 @@
 class EquipmentsController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def create
-  end
+  def create; end
 
-  def destroy
-  end
+  def destroy; end
 end
--- spec/controllers/equipments_controller_spec.rb
+++ spec/controllers/equipments_controller_spec.rb
@@ -2,35 +2,22 @@ require 'rails_helper'
 
 RSpec.describe EquipmentsController, type: :controller do
   describe 'GET #index' do
-
     context 'should return all equipments' do
-      it 'response with success' do
-      end
     end
   end
 
   describe 'GET #show' do
-
     context 'should return the equipment from id' do
-      it 'response with success' do
-      end
     end
   end
 
   describe 'POST #create' do
-
     context 'should create one equipment' do
-      it 'response with success' do
-      end
     end
   end
 
   describe 'DELETE #destroy' do
-
     context 'should delete one equipment' do
-      it 'response with success' do
-      end
     end
   end
-
 end
