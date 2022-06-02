0715868384c749f82909b81c15f82526e4e90d85
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-02 11:38:39 -0500

    Add reservations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :reservations, only: %i[index create destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ spec/requests/reservations_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Reservations', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/reservations/index'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/reservations/create'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /destroy' do
+    it 'returns http success' do
+      get '/reservations/destroy'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
