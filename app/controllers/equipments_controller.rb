9bdddbf896e893bdbffe6d19705cfe66a620720b
jose-Abel <joseabel.ramirezfrontany@gmail.com>
2022-06-03 08:36:07 -0400

    Added the equipments#destroy route. Added the EquipmentsController#destroy action

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,9 +1,15 @@
 class EquipmentsController < ApplicationController
+  include Response
+  skip_before_action :authorize_request, only: %i[index show destroy]
+
   def index; end
 
   def show; end
 
   def create; end
 
-  def destroy; end
+  def destroy
+    destroy_ctx = Equipment.find(params[:id]).destroy
+    json_response({ equipment_destroy: destroy_ctx }, 204)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :reservations, only: %i[index create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :equipment, only: %i[index show create destroy]
+  resources :equipments, only: %i[index show create destroy]
   resources :user, only: %i[show]
   post 'login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
