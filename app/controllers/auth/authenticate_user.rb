3ef811f1ab4c5fd6a6c5037ab7850011819e6fd7
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-03 11:41:37 -0500

    Add bad request responses and fix sign up bug

--- app/controllers/auth/authenticate_user.rb
+++ app/controllers/auth/authenticate_user.rb
@@ -1,11 +1,27 @@
 class Auth::AuthenticateUser
+  include Message
+
   def initialize(username, password)
     @username = username
     @password = password
   end
 
   def call
-    JsonWebToken.encode(user_id: user.id) if user
+    auth_token = JsonWebToken.encode(user_id: user.id) if user
+    if auth_token
+      return {
+        message: {
+          auth_token:
+        },
+        status: 200
+      }
+    end
+    {
+      message: {
+        error: Message.invalid_credentials
+      },
+      status: 404
+    }
   end
 
   private
