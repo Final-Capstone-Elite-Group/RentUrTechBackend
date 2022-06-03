b4c57a40b006548ce703b11677b11f931677db2b
JoseAbel <joseabel.ramirezfrontany@gmail.com>
2022-06-03 14:41:50 -0400

    Adding the seed data

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,5 +1,21 @@
 class Equipment < ApplicationRecord
   has_many :equipment_reservations, dependent: :destroy
   has_many :reservations, through: :equipment_reservations
+  has_many_attached :images
+
+  validate :acceptable_image
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+
+  def acceptable_image
+    return unless main_image.attached?
+
+    unless main_image.byte_size <= 1.megabyte
+      errors.add(:main_image, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(main_image.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
+  end
 end
