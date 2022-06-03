74913fd1527409c00933d1faf54f8a0c15fbb9d8
JoseAbel <joseabel.ramirezfrontany@gmail.com>
2022-06-03 15:53:16 -0400

    Merging with develop

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,19 @@
 class ReservationsController < ApplicationController
   def index; end
 
-  def create; end
+  def create
+    reservation_ctx = Reservations::Create.call({
+      params: reservartions_params,
+      user: @current_user  })
+
+    json_response({ reservation: reservation_ctx.reservation })
+  end
 
   def destroy; end
+
+  private
+
+  def reservartions_params
+    params.require(:reservation).permit(:total, :reserved_date, :city)
+  end
 end
