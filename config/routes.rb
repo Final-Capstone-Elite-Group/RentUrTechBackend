7e66c00df8842265f2b845dcd1b7eab99ff46a81
JoseAbel <joseabel.ramirezfrontany@gmail.com>
2022-06-03 13:27:23 -0400

    Fixing a plural for the route name of user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :reservations, only: %i[index create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :equipments, only: %i[index show create destroy]
-  resources :user, only: %i[show]
+  resources :users, only: %i[show]
   post 'login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   # Defines the root path route ("/")
