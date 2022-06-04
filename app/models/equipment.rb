f8d6d01c7a5cec27385c95bcfc37498b919e021d
jose-Abel <joseabel.ramirezfrontany@gmail.com>
2022-06-04 01:13:20 -0400

    Remove image validation since was added in the serializer

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,25 +1,5 @@
 class Equipment < ApplicationRecord
   belongs_to :user
 
-  has_many_attached :images
-
-  validate :acceptable_image
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
-
-  def acceptable_image
-    return unless images.first&.attached?
-
-    validate_images!
-  end
-
-  private
-
-  def validate_images!
-    acceptable_types = ['image/jpeg', 'image/png']
-    images.each do |img|
-      errors.add(:img, 'is too big') unless img.byte_size <= 1.megabyte
-
-      errors.add(:img, 'must be a JPEG or PNG') unless acceptable_types.include?(img.content_type)
-    end
-  end
 end
