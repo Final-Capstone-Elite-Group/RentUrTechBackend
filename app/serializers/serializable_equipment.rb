e186d16a37a9a1bca3151be2deeb8edf70433814
JoseAbel <joseabel.ramirezfrontany@gmail.com>
2022-06-06 15:42:56 -0400

    Fixig a typo for one of the fields of the equipment model

--- app/serializers/serializable_equipment.rb
+++ app/serializers/serializable_equipment.rb
@@ -3,7 +3,7 @@ class SerializableEquipment < JSONAPI::Serializable::Resource
 
   type 'equipment'
 
-  attributes :id, :title, :description, :review, :date_reserved, :duration, :rent_fee, :total_amount_payable, :url,
+  attributes :id, :title, :description, :review, :dates_reserved, :duration, :rent_fee, :total_amount_payable, :url,
              :created_at, :updated_at, :url
 
 end
