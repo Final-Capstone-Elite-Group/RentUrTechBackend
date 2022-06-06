83953e637203f6e4e9e80c14144bfa07727d9f1e
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-06 17:01:37 -0500

    Define index interactor

---
+++ app/interactors/reservations/index.rb
@@ -0,0 +1,25 @@
+class Reservations::Index
+  include Interactor
+
+  def call
+    context.reservations = query&.reorder(order)
+  rescue StandardError => e
+    handle_errors(e.message)
+  end
+
+  private
+
+  def query
+     @reservations = context.params[:user].reservations.includes(:equipment)
+  end
+
+  def order
+    {
+      created_at: :desc
+    }
+  end
+
+  def handle_errors(message, status = 422)
+    context.fail!(errors: message, status:)
+  end
+end
