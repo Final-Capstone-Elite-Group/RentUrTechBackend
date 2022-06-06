1160aca48ccc2e9f28bed6104c6c6add64e2f0f8
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-06 17:38:47 -0500

    Fix origin in host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001/"
+    origins "localhost:3000"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
