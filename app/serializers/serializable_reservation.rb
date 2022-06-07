89b72dba8c14843ffe6a51bf40ba0b976a310bb3
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-07 16:59:08 -0500

    Modify serializer to use new url column and save queries

--- app/serializers/serializable_reservation.rb
+++ app/serializers/serializable_reservation.rb
@@ -6,26 +6,6 @@ class SerializableReservation < JSONAPI::Serializable::Resource
   attributes :id, :total, :reserved_date, :city, :created_at, :updated_at
 
   attribute :equipment do
-    @object.equipment.slice('id', 'title')
-  end
-
-  attribute :equipment_image do
-    modify_image
-  end
-
-  private
-
-  def modify_image
-    equipment = @object.equipment
-    return unless equipment.image.attached?
-
-    equipment.image.blob.attributes
-      .slice('filename')
-      .merge(url: image_url)
-      .tap { |attrs| attrs['name'] = attrs.delete('filename') }
-  end
-
-  def image_url
-    url_for(@object.equipment.image)
+    @object.equipment.slice('id', 'title', 'url')
   end
 end
