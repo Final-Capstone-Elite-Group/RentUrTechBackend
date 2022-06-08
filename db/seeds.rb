203bcd42952df833831901a8c61ada9317b09c65
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-08 10:12:15 -0500

    Modify duration of equipment

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,7 @@ three_d_printer_ender = Equipment.new({
   title: "3D Printer Ender",
   description: "An open-source 3d printer with amazing printing precision and affordable price, quite the best 3d printer for beginners. Allow users to improve their printer upon the source code and share the changes within the community, resulting in an improved experience for all.",
   review: "https://www.youtube.com/watch?v=QEzMWE3-lVY",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -46,7 +46,7 @@ three_d_printer_anet = Equipment.new({
   title: "3D Printer Anet",
   description: "Offering a step-by-step learning experience for a 3D printer kit, the Anet A8 features a black laser-cut acrylic frame and 220 x 220 x 240mm printing volume. It's compatible with a number of materials, including ABS, PLA, Woodfill, Nylon PVA, PP, and others.",
   review: "https://www.youtube.com/watch?v=3qlpdI1kTV4",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -61,7 +61,7 @@ airtag = Equipment.new({
   title: "Airtag",
   description: "AirTag is a supereasy way to keep track of your stuff. Attach one to your keys. Put another in your backpack. And just like that, they’re on your radar in the Find My app, where you can also track down your Apple devices and keep up with friends and family.",
   review: "https://www.youtube.com/watch?v=ehv3zQAa9zM",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -76,7 +76,7 @@ drone1 = Equipment.new({
   title: "GPS Drone",
   description: "A flying robot that can be remotely controlled or fly autonomously using software-controlled flight plans in its embedded systems, that work in conjunction with onboard sensors and a global positioning system (GPS).",
   review: "https://www.youtube.com/watch?v=kg7fngZM3PI",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -91,7 +91,7 @@ drone2 = Equipment.new({
   title: "Amazing Drone",
   description: "This Drone have many functions, ranging from monitoring climate change to carrying out search operations after natural disasters, photography, filming, and delivering goods. Intelligent Interactive Drone: The drone is integrated with smart voice control and gesture control. You can speak to control the flying direction, or pose to the camera to trigger photo/video shooting, adding fun to your flight.",
   review: "https://www.youtube.com/watch?v=nl1rwLe8dWY",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -106,7 +106,7 @@ drone3 = Equipment.new({
   title: "Quadcore Drone",
   description: "this drone is an extremely well-built ready-to-fly racing drone with all of the features that beginners and pros need. The first thing you’ll notice is the cleaner design. All of the electronics are sandwiched between the carbon fiber on the top and the printed circuit board on the bottom. The only thing that sticks out is the camera which is protected by a hard plastic case.",
   review: "https://www.youtube.com/watch?v=2TNfl4COLgA",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -122,7 +122,7 @@ home_theater = Equipment.new({
   description: "An entertainment system for the home that usually consists of a large television with video components (such as a DVD player and VCR) and an audio system offering surround sound.Low-Profile Design and Brilliant Clarity with 6 Full-range Speakers
   Powerful 5. Wireless Subwoofer and Bluetooth Music Streaming. Complete Immersion with Discreet Volume Adjustable Surround Speakers. Optimized for 4K TVs with Dolby Audio 5.1 and HDMI (Cable Included). Control with TV Remote using HDMI, Audio Cables Included for Easy Setup",
   review: "https://www.youtube.com/watch?v=gxABNCBFejo",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -137,7 +137,7 @@ leather_touchscreen_gloves = Equipment.new({
   title: "Touchscreen Gloves",
   description: "Crafted with poise and quality, the gloves present characteristics of masculinity with carefully considered materials. The matte finishing of the leather allows for a well-balanced iconic look in uniform dark tones along with a few areas of contrast intensifying the low-key design.",
   review: "https://www.youtube.com/watch?v=dJPt36E-iT0",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -153,7 +153,7 @@ microsoft_oculus = Equipment.new({
   description: "Oculus Rift S Virtual Reality System is available for purchase now. Check out VR gaming, immerse yourself in your favorite movies, or bring friends to play with Oculus Rift S using Windows 10. Keep your experience smooth and seamless, even as high speed action unfolds around you with a super-fast processor and high-resolution display. (Packaging may vary) Meta Quest packaging will continue to carry the Oculus name and logo during the transition to our new branding. Experience total immersion with 3D positional audio, hand tracking and haptic feedback, working together to make virtual worlds feel real.
   Explore an expanding universe of over 250 titles across gaming, fitness, social/multiplayer and entertainment, including exclusive blockbuster releases and totally unique VR experiences",
   review: "https://www.youtube.com/watch?v=t0q8vWLjkyo",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -168,7 +168,7 @@ oculus = Equipment.new({
   title: "Oculus",
   description: "Lightweight, comfortable, and powerful enough to run impressively detailed virtual reality experiences, the Oculus Quest 2 is the best VR headset that Oculus has made so far. And, depending on your perspective on raw power versus portability and comfort, it might just be the best VR headset ever.",
   review: "https://www.youtube.com/watch?v=e5y6AycNc2E",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -186,7 +186,7 @@ powerfull_pc = Equipment.new({
   ✔ 802.11AC Wi-Fi | No Bloatware | Graphics Ports may Vary | HD Audio and Mic | Free Gaming Keyboard and Mouse | 2 x USB 3.0, 2 x USB 2.0, 4 x USB 3.2 Gen1
   ✔ 3 x RGB RING Fans for Maximum Air Flow | Skytech Archangel Gaming Case with Tempered Glass - White",
   review: "https://www.youtube.com/watch?v=Qa0jZnrQrIA",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -201,7 +201,7 @@ precision_cooker = Equipment.new({
   title: "Precision Cooker",
   description: "Perfect results every time: Never over or under cook your food again. The Precision Cooker Nano circulates water at the exact temperature required for perfectly cooked meals, no matter what's on your menu.",
   review: "https://www.youtube.com/watch?v=PN8m9KAQJ0c",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -215,7 +215,7 @@ turntable = Equipment.new({
   title: "Turntable",
   description: "Whether you’re just getting into records, rediscovering a record collection long since buried away or simply adding a turntable to complete your system, the AT-LP60X fully automatic belt-drive turntable is an excellent choice. This affordable, easy-to-use turntable plays both 33-1/3 and 45 RPM records and, with its built-in switchable phonon preamp, can be connected directly to your home stereo, powered speakers, computer and other components, whether they have a dedicated phonon input or not. An update of the popular AT-LP60, the ATLP60X features a redesigned tone arm base and head shell to improve tracking and reduce resonance. Noise is further reduced by the inclusion of an AC adapter that moves the AC/DC conVersion outside of the chassis where its impact on the signal chain is limited. Audio-Technical has been a leader in phonon cartridge design for more than 50 years, and that expertise shows in the AT-LP60X’s Dual Magnet cartridge with replaceable stylus. The cartridge is integrated into the head shell for assured performance and ease of setup. The turntable comes with a detachable RCA output cable (3. 5 mm male to dual RCA male), 45 RPM adapter, and a removable hinged dust cover. It is available in black (AT-LP60X-BK), brown/black (AT-LP60X-BW), gunmetal/black (AT-LP60X-GM), and red/black (AT-LP60X-RD).",
   review: "https://www.youtube.com/watch?v=Yaj1Mhj8mo4",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
@@ -230,7 +230,7 @@ wireless_printer = Equipment.new({
   title: "Wireless Printer",
   description: "The Wireless Printer gives you all the essential features you need—easily print, scan and copy, and activate HP+: HP's smart printing system that comes with 6 months of free Instant Ink and a 2 year extended HP warranty.",
   review: "https://www.youtube.com/watch?v=9Xk39LNg304",
-  duration: FFaker::Random.rand(60),
+  duration: FFaker::Random.rand(5..10),
   rent_fee: FFaker::Random.rand(1000),
   total_amount_payable: FFaker::Random.rand(100),
   user: User.find_by(role: 'admin')
