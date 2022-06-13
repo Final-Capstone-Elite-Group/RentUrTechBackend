8220dbce223390f3760456742340c5ca0ab3f154
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-13 15:40:04 -0500

    Separate concerns in validations

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -9,11 +9,10 @@ class Equipment < ApplicationRecord
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :description, presence: true, length: { minimum: 200, maximum: 2000 }
   validates :review, presence: true
-  validates :duration, presence: true
-  validates :rent_fee, presence: true
-  validates :total_amount_payable, presence: true
+  validates :duration, presence: true, numericality: { greater_than: 0 }
+  validates :rent_fee, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :total_amount_payable, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :image, presence: true
-  validate :proper_numeric_inputs?
 
   private
 
@@ -27,15 +26,5 @@ class Equipment < ApplicationRecord
 
   def image_url
     url_for(image)
-  end
-
-  def proper_numeric_inputs?
-    errors.add(:rent_fee, 'should exist and be greater than or equal to 0') unless rent_fee && rent_fee >= 0
-
-    unless total_amount_payable && total_amount_payable >= 0
-      errors.add(:total_amount_payable, 'should exist and be greater than or equal to 0')
-    end
-
-    errors.add(:duration, 'should exist and be greater than 0') unless duration&.positive?
   end
 end
